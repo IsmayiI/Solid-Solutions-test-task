33b24e9ef1e3bd97a553ef6c716c25d5669bb972
Ismayil Ismayilov <Psixo2015@mail.ru>
2024-01-22 12:47:50 +0400

    add comments

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+   // переменные
 
    const createRootBtn = document.getElementById('createRoot')
    const tree = document.getElementById('tree')
+
+   // функция по созданию кнопки
 
    function createBtn(classes, elem, node) {
       const btn = document.createElement('button');
@@ -11,50 +14,69 @@ document.addEventListener('DOMContentLoaded', function () {
       node.append(btn);
    }
 
+   // функция по созданию узла
+
    function createNode(parent) {
+
+      // создание узла
       const node = document.createElement('div');
       node.className = 'node active';
 
+      // Создание кнопок
       createBtn('btn btn-secondary toggle-btn d-none', '&#8600;', node)
       createBtn('btn btn-success mr-2 add-node', '+', node)
       createBtn('btn btn-danger mr-2 remove-node', '-', node)
 
+      // Создание текстового элемента
       const text = document.createElement('span');
       text.className = 'node-text';
       text.textContent = generateRandomString();
       node.append(text);
 
+      // Создание линии вложенности
       const line = document.createElement('div');
       line.className = 'line';
       parent.append(line);
 
+      // Добавление узла к родительскому элементу
       parent.append(node);
 
    }
 
+   // работа с кнопкой "Create Root"
 
    createRootBtn.addEventListener('click', function () {
       createNode(tree);
       updateLocalStorage();
    });
 
+   // работа с деревом узлов
+
    tree.addEventListener('click', function (e) {
       const target = e.target;
       const parentNode = target.parentNode
 
+      // работа при клике на "добавить"
       if (parentNode.classList.contains('active') && target.classList.contains('add-node')) {
-         const childNode = parentNode.querySelector('.toggle-btn');
-         childNode.classList.remove('d-none');
+         const toggleBtn = parentNode.querySelector('.toggle-btn');
+         toggleBtn.classList.remove('d-none');
 
          createNode(parentNode);
          updateLocalStorage();
       }
 
+      // работа при клике на "удалить"
       if (target.classList.contains('remove-node')) {
+         const childNodes = parentNode.parentNode.querySelectorAll('.node')
+         const toggleBtn = parentNode.parentNode.querySelector('.toggle-btn');
+         if (childNodes.length <= 1) {
+            toggleBtn.classList.add('d-none');
+         }
          parentNode.remove();
          updateLocalStorage();
       }
 
+      // работа при клике на "свернуть"
       if (target.classList.contains('toggle-btn')) {
          parentNode.classList.toggle('active');
 
@@ -65,6 +87,8 @@ document.addEventListener('DOMContentLoaded', function () {
          target.innerHTML = parentNode.classList.contains('active') ? '&#8600;' : '&#8594;';
       }
    });
+
+   // работа с локальным хранилищем
 
    function updateLocalStorage() {
       const treeData = tree.innerHTML;
@@ -77,6 +101,8 @@ document.addEventListener('DOMContentLoaded', function () {
    }
 
 
+   // функция по созданию случайной строки из трёх букв
+
    function generateRandomString() {
       const alphabet = 'abcdefghijklmnopqrstuvwxyz';
       let randomString = '';
@@ -88,4 +114,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
       return randomString;
    }
+
+
 });
